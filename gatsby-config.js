88ecad1a36ef260d90342fb1e84b6367ca53ca36
williammiranda <w.miranda89@gmail.com>
2020-05-11 08:25:20 -0400

    variable change in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,9 +31,9 @@ module.exports = {
          * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
          * Example : 'demo.wp-api.org' or 'www.example-site.com'
          */
-        baseUrl: "portdemo.local",
+        baseUrl: process.env.API_URL,
         // The protocol. This can be http or https.
-        protocol: "https",
+        protocol: process.env.API_PROTOCOL,
         // The rest api route prefix that your WordPress site is using.
         // Sometimes this is modified by WordPress plugins.
         // If not set, it uses the default of "wp-json"
